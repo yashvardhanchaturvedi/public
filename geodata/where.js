ab474eb14a7574788b5ee70083e94dccbaa8cf70
yashvardhanchaturvedi <42029180+yashvardhanchaturvedi@users.noreply.github.com>
2020-09-16 18:29:24 +0530

    Add files via upload

---
+++ geodata/where.js
@@ -0,0 +1,16 @@
+myData = [
+[50.0676785,19.9070824, 'Dormitory no. 14 "Kapitol" of AGH University of Science and Technology in Cracov, Poland, Witolda Budryka 2, 30-072 Kraków, Poland'],
+[52.2296756,21.0122287, 'Warsaw, Poland'],
+[30.0068972,31.5005988, 'American University Housing District, Cairo Governorate, Egypt'],
+[33.4232051,-111.8879509, 'State Ave, Tempe, AZ 85281, USA'],
+[33.9094658,-83.4604372, 'Information Technology Services, 2500 Daniels Bridge Rd, Athens, GA 30606, USA'],
+[28.3580163,75.5887989, 'BITS, Pilani, Rajasthan 333031, India'],
+[43.1294077,-77.5479304, 'Babcock Dr, Rochester, NY 14610, USA'],
+[25.2685304,82.9904737, 'Banaras Hindu University Campus, Varanasi, Uttar Pradesh, India'],
+[12.9426914,77.5053145, 'Bangalore University Rd, Bengaluru, Karnataka 560056, India'],
+[64.8852156,-147.8009743, 'Baylor Blvd, Fairbanks, AK 99709, USA'],
+[39.9304437,116.3076925, 'Capital Normal University, Haidian, Beijing, China, 100037'],
+[53.89318369999999,27.547338, 'Monument to Fallen Professors and Students of the Belarusian State University, Minsk, Belarus'],
+[33.0430635,-96.72549579999999, 'Belgrade Dr, Plano, TX 75023, USA'],
+[42.5030209,-89.0295642, 'College St, Beloit, WI 53511, USA']
+];
